b90cde1d4a952b48af25014bd69f5ab986dcaf82
littleprince415 <mchayanut@kkumail.com>
2022-08-15 17:32:19 +0700

    updateCodeP2

--- lab5/p2.js
+++ lab5/p2.js
@@ -1,17 +1,31 @@
-let arrayInput = [1,2,3,4];
-let userInput = 0;
-while (userInput >= 0) {
-    userInput = Number(prompt("Enter a positive integer"));
-    if(userInput < 0){
+let arrayInput = [];
+let userAnswer = 0;
+while (true) {
+    let userAnswer = Number(prompt("Enter a positive integer"));
+    if(userAnswer < 0){
         break;
     } 
     else {
-        if (!isNaN(userInput)) {
-            arrayInput.push(userInput);
+        if (Number.isInteger(userAnswer) && !isNaN(userAnswer)) {
+            arrayInput.push(userAnswer);
         }
     }
 }
 let avg = arrayInput.reduce((pre, curr) => pre + curr, 0)/arrayInput.length;
 let min = Math.min(...arrayInput);
 let max = Math.max(...arrayInput);
-alert("for n = " +(arrayInput.length)+ " the average is " + avg + " the minimum is " + min + " the maximum is " + max)
+alert("for n = " +(arrayInput.length)+ " the average is " + avg.toFixed(2) + " the minimum is " + min + " the maximum is " + max)
+
+function showPrimes(userAnswer) {
+    for (i = 2; i < userAnswer; i++) {
+        if (!isPrime(i)) continue;
+        factorArrey.push(i);
+    }
+    return factorArrey
+}
+function isPrime(userAnswer) {
+    for (let i = 2; i < userAnswer; i++) {
+        if (userAnswer % i == 0) return false;
+    }
+    return true;
+}
